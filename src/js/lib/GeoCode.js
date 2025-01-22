7266d2451a87382453e9de3b323ab5d347f6c55d
venkatachalampradeep <pradeep.venkatachalam1495@gmail.com>
2025-01-22 20:18:50 +0100

    Update Geocode

--- src/js/lib/GeoCode.js
+++ src/js/lib/GeoCode.js
@@ -21,35 +21,6 @@ function reverseGeocoding(lat, lon, callback) {
             }
         };
 
-        /*
-        request.onload = function() {
-
-            if (request.status == 200){
-
-                let data = JSON.parse(request.responseText);
-                let _output = []
-                    if (!!data.address.suburb) { _output.push(data.address.suburb) }
-                    if (!!data.address.city_district) { _output.push(data.address.city_district) }
-                    if (!!data.address.city) { _output.push(data.address.city) }
-
-                callback(_output.join(', '))
-
-                // return _output.join(', ');
-                // console.log(data.address.suburb, data.address.city_district, data.address.city);
-                // console.log(data.results[0].formatted);
-
-            } else {
-                return false;
-            }
-            // } else if (request.status <= 500){
-            //
-            //     console.log("unable to geocode! Response code: " + request.status);
-            //     var data = JSON.parse(request.responseText);
-            //     console.log(data.status.message);
-            //
-            // } else { console.log("server error") }
-        };
-        */
         request.onerror = function() {
             return false;
             // console.log("unable to connect to server")
