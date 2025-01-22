d39c3505db1aca3c88a3fb3ace186ddc2f475acb
venkatachalampradeep <pradeep.venkatachalam1495@gmail.com>
2025-01-22 19:24:11 +0100

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,18 +1,8 @@
 const Config = {
-    title : 'InstaGPX · Add GPX activity stats to your photos',
-    description : 'Create beautiful sharing pictures adding activity stats from a GPX file to your photos with InstaGPX',
+    title : 'InstaGPX',
+    description : '',
     url : 'https://instagpx.com',
     timestampTemplates : [
-        '{dddd}, {DD}.{Mo}.{YYYY} @{H}:{mm}',
-        '{dddd}, {DD} {MMMM} {YYYY} · {h}:{mm}{a}',
-        '{dddd}, {MMMM} {DD}, {YYYY} · {h}:{mm}{a}',
-        '{DD}.{Mo}.{YYYY} · {H}:{mm}',
-        '{Mo}.{DD}.{YYYY} · {H}:{mm}',
-        '{Do} {MMMM} {YYYY}',
-        '{DD} {MMMM} {YYYY}',
-        '{DD} {MM} {YYYY} · {h}:{mm}{a}',
-        '{MMMM} {Do}, {YYYY}',
-        '{MMMM} {DD}, {YYYY}'
     ],
     imageModes: {
         '1:1' : { width: 1280, height: 1280, name: '1:1  Square'    },
